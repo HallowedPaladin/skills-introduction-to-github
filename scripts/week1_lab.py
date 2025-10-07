4dafeac51d33eb75b12287ba9dd60d9f44ea5bb4
HallowedPaladin <lukezgwilliams@gmail.com>
2025-10-07 15:21:08 +0100

    Feat: Complete Week 1 lab exercises

---
+++ scripts/week1_lab.py
@@ -0,0 +1,17 @@
+print("Hello, World")
+
+# Storing text (a string)
+student_name = "Luke"
+
+# Storing a whole number (an integer)
+student_age = 19
+
+# Print the variables to the console
+print("Student Name:", student_name)
+print("Student Age:", student_age)
+
+# A simple calculation
+num1 = 27
+num2 = 14
+total = num1 + num2
+print("The sum is:", total)
